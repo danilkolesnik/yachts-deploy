0676dc64a5b12f6818a542db66f8136f9c3c94d0
danilkolesnik <apparentlysolved@gmail.com>
2025-03-24 16:27:16 +0200

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -28,32 +28,32 @@ import { join } from 'path';
         }),
 
       ],
-      // useFactory:() =>({
-      //   type:"postgres",
-      //   host:"ep-twilight-sun-a2y5dnis.eu-central-1.pg.koyeb.app",
-      //   port:5432,
-      //   username:"koyeb-adm",
-      //   password:"npg_HN76ryMJUTiK",
-      //   database:"koyebdb",
-      //   synchronize:true,
-      //   entities: [__dirname + '/**/*.entity{.js, .ts}'],
-      //   ssl: {
-      //     rejectUnauthorized: false,
-      //   },
-      // })
       useFactory:() =>({
         type:"postgres",
-        host:process.env.DATABASE_HOST,
+        host:"ep-twilight-sun-a2y5dnis.eu-central-1.pg.koyeb.app",
         port:5432,
-        username:process.env.DATABASE_USER,
-        password:process.env.DATABASE_PASSWORD,
-        database:process.env.DATABASE_NAME,
+        username:"koyeb-adm",
+        password:"npg_HN76ryMJUTiK",
+        database:"koyebdb",
         synchronize:true,
         entities: [__dirname + '/**/*.entity{.js, .ts}'],
-        // ssl: {
-        //   rejectUnauthorized: true,
-        // },
+        ssl: {
+          rejectUnauthorized: false,
+        },
       })
+      // useFactory:() =>({
+      //   type:"postgres",
+      //   host:process.env.DATABASE_HOST,
+      //   port:5432,
+      //   username:process.env.DATABASE_USER,
+      //   password:process.env.DATABASE_PASSWORD,
+      //   database:process.env.DATABASE_NAME,
+      //   synchronize:true,
+      //   entities: [__dirname + '/**/*.entity{.js, .ts}'],
+      //   // ssl: {
+      //   //   rejectUnauthorized: true,
+      //   // },
+      // })
     }),
     AuthModule, 
     WarehouseModule, 
