465de7995041ea94642869e86a5afebe989b73e8
danilkolesnik <apparentlysolved@gmail.com>
2025-06-15 18:22:09 +0300

    fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -66,7 +66,7 @@ createUploadDirectories();
         username:process.env.DATABASE_USER,
         password:process.env.DATABASE_PASSWORD,
         database:process.env.DATABASE_NAME,
-        synchronize: false,
+        synchronize: true,
         entities: [
           users,
           File,
