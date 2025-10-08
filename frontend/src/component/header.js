bc1eeaf4dfa851f0b387ccb236b29271afedc409
unknown <apparentlysolved@gmail.com>
2025-10-08 18:18:48 +0200

    fix: header style

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -71,7 +71,7 @@ function NavList({ isOpen, setIsOpen }) {
     <List className={`flex items-center ${isMobile ? 'flex-col' : 'flex-row'} w-full p-0`}>
       <Link href="/offers" onClick={handleClick} className="font-bold">
         <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium`}>
-            <DocumentTextIcon className={`h-5 w-5 mr-2 ${pathname === '/offers' ? 'text-[#dd3333]' : 'text-black'}`} />
+            <DocumentTextIcon className={`h-5 w-5 mr-2 text-black`} />
           <span
           style={{ color: pathname === '/offers' ? '#dd3333' : 'black' }}
           >Offers</span>
