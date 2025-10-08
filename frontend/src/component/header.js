addd5b5a98fe0433a984b726187e1c5096ce0edd
unknown <apparentlysolved@gmail.com>
2025-10-08 15:07:41 +0200

    fix: header

--- frontend/src/component/header.js
+++ frontend/src/component/header.js
@@ -19,6 +19,7 @@ import {
 import { useAppDispatch } from '@/lib/hooks';
 import { setId } from '@/lib/features/todos/usersDataSlice';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import Loader from '@/ui/loader';
 import Image from 'next/image';
 import logo from '../../public/logo/logo.svg';
@@ -29,6 +30,7 @@ function NavList({ isOpen, setIsOpen }) {
   const [role, setRole] = useState(null);
   const [isMobile, setIsMobile] = useState(false);
   const dispatch = useAppDispatch();
+  const pathname = usePathname();
 
   useEffect(() => {
     const storedRole = localStorage.getItem('role');
@@ -70,13 +72,13 @@ function NavList({ isOpen, setIsOpen }) {
       <Link href="/offers" onClick={handleClick} className="font-bold">
         <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
           <DocumentTextIcon className="h-5 w-5 mr-2" />
-          <span className="text-red-600">Offers</span>
+          <span className={pathname === '/offers' ? 'text-red-600' : 'text-black'}>Offers</span>
         </ListItem>
       </Link>
       <Link href="/orders" onClick={handleClick} className=" font-bold">
         <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
           <ArchiveBoxIcon className="h-5 w-5 mr-2" />
-          <span>Orders</span>
+          <span className={pathname === '/orders' ? 'text-red-600' : 'text-black'}>Orders</span>
         </ListItem>
       </Link>
       {(role === 'admin' || role === 'manager') && (
@@ -84,31 +86,31 @@ function NavList({ isOpen, setIsOpen }) {
           <Link href="/yachts" onClick={handleClick} className=" font-bold">
             <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
               <Image src={yacht} alt="yacht" width={38} height={38} className="mr-2" />
-              <span>Yachts</span>
+              <span className={pathname === '/yachts' ? 'text-red-600' : 'text-black'}>Yachts</span>
             </ListItem>
           </Link>
           <Link href="/warehouse" onClick={handleClick} className=" font-bold">
             <ListItem className={`flex items-center gap-2 ${isMobile ? 'w-full justify-center' : 'pr-4'} font-medium text-black`}>
               <ArchiveBoxIcon className="h-5 w-5 mr-2" />
-              <span>Warehouse</span>
+              <span className={pathname === '/warehouse' ? 'text-red-600' : 'text-black'}>Warehouse</span>
             </ListItem>
           </Link>
           <Link href="/warehouseUnofficially" onClick={handleClick} className=" font-bold"> 
             <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
               <ArchiveBoxIcon className="h-5 w-5 mr-2" />
-              <span>Internal warehouse</span>
+              <span className={pathname === '/warehouseUnofficially' ? 'text-red-600' : 'text-black'}>Internal warehouse</span>
             </ListItem>
           </Link>
           <Link href="/priceList" onClick={handleClick} className=" font-bold">
             <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
               <CurrencyDollarIcon className="h-5 w-5 mr-2" />
-              <span>Price List</span>
+              <span className={pathname === '/priceList' ? 'text-red-600' : 'text-black'}>Price List</span>
             </ListItem>
           </Link>
           <Link href="/users" onClick={handleClick} className=" font-bold">
             <ListItem className={`flex items-center gap-2 py-2 pr-4 font-medium text-black`}>
               <UserIcon className="h-5 w-5 mr-2" />
-              <span>Users</span>
+              <span className={pathname === '/users' ? 'text-red-600' : 'text-black'}>Users</span>
             </ListItem>
           </Link>
         </>
