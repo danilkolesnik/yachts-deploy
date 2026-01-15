a7586ed1509a102263e19e1177bd75838512ed75
unknown <apparentlysolved@gmail.com>
2026-01-15 10:58:48 +0100

    fix: offers history

--- frontend/src/app/offers/page.js
+++ frontend/src/app/offers/page.js
@@ -1639,7 +1639,7 @@ const OfferPage = () => {
                                     name="date"
                                     value={historyFilters.date}
                                     onChange={handleHistoryFilterChange}
-                                    className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                    className="w-full border border-gray-300 px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
                                 />
                             </div>
                             
